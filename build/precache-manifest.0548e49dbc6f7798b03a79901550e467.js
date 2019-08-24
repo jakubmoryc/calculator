cfc14612ff8d9dc49b5653ca5ae26b240215b84d
Mortzcent <moryckuba@gmail.com>
2019-08-24 12:41:03 +0200

    testing ssh

---
+++ build/precache-manifest.0548e49dbc6f7798b03a79901550e467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23596418c9054c8a730466fb25f137d0",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7414fd282993cd3a53b7",
+    "url": "/calculator/static/css/main.0ca27f51.chunk.css"
+  },
+  {
+    "revision": "fbb65b86253c03bc6240",
+    "url": "/calculator/static/js/2.91db048d.chunk.js"
+  },
+  {
+    "revision": "7414fd282993cd3a53b7",
+    "url": "/calculator/static/js/main.f67a962f.chunk.js"
+  },
+  {
+    "revision": "9c7fd898fe59b4186d5d",
+    "url": "/calculator/static/js/runtime~main.4df7f200.js"
+  }
+]);
