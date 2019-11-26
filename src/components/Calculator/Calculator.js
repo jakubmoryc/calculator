36d5247888857df1db8919100b6911ba0911dd0b
Mortzcent <moryckuba@gmail.com>
2019-11-26 18:03:18 +0100

    fixed mobile rwd

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -137,6 +137,15 @@ export default class Calculator extends Component {
         Calculator.vibrate();
     }
 
+    componentDidMount() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        window.addEventListener('resize', () => {
+            // We execute the same script as before
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+          })
+    }
 
     render() {
         return (
