3181689089852c5d80dc4c15f79a0334f74e8e76
Mortzcent <moryckuba@gmail.com>
2019-11-26 18:04:38 +0100

    Updates

--- precache-manifest.02b5bc7e1d5ac5bc2d3039bafb1c134e.js
+++ precache-manifest.02b5bc7e1d5ac5bc2d3039bafb1c134e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8bf6f2a9bf5721acb46846155a57d604",
+    "revision": "a12d11b889f4dc322001042cec70a913",
     "url": "/calculator/index.html"
   },
   {
-    "revision": "c855d1535f4a26aa8b38",
-    "url": "/calculator/static/css/main.0aec9edf.chunk.css"
+    "revision": "7e6f85a50d382d89daf7",
+    "url": "/calculator/static/css/main.4b1b5463.chunk.css"
   },
   {
     "revision": "6354e836bf8a520775a0",
     "url": "/calculator/static/js/2.38773eed.chunk.js"
   },
   {
-    "revision": "c855d1535f4a26aa8b38",
-    "url": "/calculator/static/js/main.dbec207a.chunk.js"
+    "revision": "7e6f85a50d382d89daf7",
+    "url": "/calculator/static/js/main.75b50388.chunk.js"
   },
   {
     "revision": "9c7fd898fe59b4186d5d",
